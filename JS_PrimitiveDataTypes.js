f6cf8c56de1f1258115151f47f55b84ea9e9a4cd
Muhammad Fahad <92125039+MuhammadFahad1249@users.noreply.github.com>
2024-04-20 14:28:14 +0500

    Add files via upload

---
+++ JS_PrimitiveDataTypes.js
@@ -0,0 +1,14 @@
+//  nn         |   bb           |    ss         |      u 
+// null,number | boolean,BIgint | String,Symbol | Undefined
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567")+BigInt("5")
+let e = "Fahad"
+let f = Symbol("I am fahad")
+let g = undefined
+let h // it is also a undefined variable
+
+console.log(a,b,c,d,e,f,g,h)
+console.log(typeof(f))
