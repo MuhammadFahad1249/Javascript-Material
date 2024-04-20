111564229d18bf150e42432bb5fb6b088ee569bd
Muhammad Fahad <92125039+MuhammadFahad1249@users.noreply.github.com>
2024-04-20 14:41:50 +0500

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,14 @@
+// In JS Objects are same as Python Dictionaries , they have an key value pair 
+// Objects are used for Lookups
+// Objects are the Non primitive Data type in Javascript
+
+
+const item = 
+{
+    "Mobile" : "Iphone",
+    "Mouse" : "LogiTech",
+    "MobilePrice" : 100000
+
+}
+
+console.log(item[("Mobile")])
